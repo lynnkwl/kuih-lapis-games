7321d2631a3ff245abe6b72f5b854b1f4745a9d2
HunchoQuavo888 <ralph.ong.888@gmail.com>
2024-03-26 01:10:04 +0800

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
-export default class Profile extends Component {
+class Profile extends Component {
   render() {
     return (
       <div>
-        Profile
+        <h2>Profile</h2>
+        <UserProfile />
       </div>
-    )
+    );
   }
 }
+
+function UserProfile() {
+  const { isAuthenticated, user } = useAuth0();
+
+  if (!isAuthenticated) {
+    return <div>Please log in to view your profile.</div>;
+  }
+
+  return (
+    <div>
+      <p>Welcome, {user.name}!</p>
+      <p>Your email: {user.email}</p>
+      <p>Your user ID: {user.sub}</p>
+    </div>
+  );
+}
+
+export default Profile;
